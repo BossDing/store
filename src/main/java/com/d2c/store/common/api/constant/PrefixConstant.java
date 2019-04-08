0efd093ca5685f73eda58b644d3ee6aaa5a7fed0
lain <405437980@qq.com>
2019-04-08 14:14:47 +0800

    法大大业务

--- src/main/java/com/d2c/store/common/api/constant/PrefixConstant.java
+++ src/main/java/com/d2c/store/common/api/constant/PrefixConstant.java
@@ -9,5 +9,7 @@ public interface PrefixConstant {
      * 订单编号前缀
      */
     String ORDER_PREFIX = "T";
+    String FDD_TRANSATION_PREFIX = "FT";
+    String FDD_APPLYNUM_PREFIX = "FA";
 
 }
--- src/main/java/com/d2c/store/modules/core/model/P2PDO.java
+++ src/main/java/com/d2c/store/modules/core/model/P2PDO.java
@@ -40,5 +40,25 @@ public class P2PDO extends BaseDelDO {
     @Assert(type = AssertEnum.NOT_NULL)
     @ApiModelProperty(value = "授权有效期-小时")
     private Integer oauthTime;
+    @ApiModelProperty(value = "法大大客户编号")
+    private String customerId;
+    @ApiModelProperty(value = "企业存证编号")
+    private String evidenceNo;
+    @ApiModelProperty(value = "信用代码")
+    private String creditCode;
+    @ApiModelProperty(value = "信用代码电子版")
+    private String creditCodeFile;
+    @ApiModelProperty(value = "委托书电子版")
+    private String powerAttorneyFile;
+    @ApiModelProperty(value = "法人名字")
+    private String legalName;
+    @ApiModelProperty(value = "法人身份证")
+    private String identity;
+    @ApiModelProperty(value = "地址")
+    private String address;
+    @ApiModelProperty(value = "签章图片")
+    private String signId;
+    @ApiModelProperty(value = "法人客户编号")
+    private String legalCustomerId;
 
 }
