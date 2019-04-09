00ba9e98a81642f02f5ce714185a9d3707b8d46c
lain <405437980@qq.com>
2019-04-09 18:10:43 +0800

    法大大业务

--- src/main/java/com/d2c/store/api/C_MemberController.java
+++ src/main/java/com/d2c/store/api/C_MemberController.java
@@ -74,23 +74,26 @@ public class C_MemberController extends BaseController {
     @RequestMapping(value = "/fadada/apply", method = RequestMethod.POST)
     public R<MemberDO> fadadaApply() {
         MemberDO memberDO = loginMemberHolder.getLoginMember();
-        MemberDO member = new MemberDO();
-        member.setId(memberDO.getId());
         Asserts.notNull("请先维护个人信息，真实姓名，手机，身份证不能为空", memberDO.getNickname(), memberDO.getIdentity(), memberDO.getAccount());
         if (memberDO.getCustomerId() == null) {
             String customerId = fadadaClient.registerAccount(PrefixConstant.FDD_PERSON_ACCOUNT_PREFIX + memberDO.getId(), "1");
-            member.setCustomerId(customerId);
+            memberDO.setCustomerId(customerId);
         }
         if (memberDO.getEvidenceNo() == null) {
             String evidenceNo = fadadaClient.personDeposit(memberDO, PrefixConstant.FDD_PERSON_APPLY_PREFIX + memberDO.getId());
-            member.setEvidenceNo(evidenceNo);
+            memberDO.setEvidenceNo(evidenceNo);
             fadadaClient.applyClinetNumcert(memberDO.getCustomerId(), memberDO.getEvidenceNo());
         }
         if (memberDO.getSignId() == null) {
             String singImg = fadadaClient.customSignature(memberDO.getCustomerId(), memberDO.getNickname());
             String signId = fadadaClient.addSignature(memberDO.getCustomerId(), singImg);
-            member.setSignId(signId);
+            memberDO.setSignId(signId);
         }
+        MemberDO member = new MemberDO();
+        member.setId(memberDO.getId());
+        member.setCustomerId(memberDO.getCustomerId());
+        member.setEvidenceNo(memberDO.getEvidenceNo());
+        member.setSignId(memberDO.getSignId());
         memberService.updateById(member);
         return Response.restResult(memberDO, ResultCode.SUCCESS);
     }
