a0955919cb74ea4ee7c9f1c006d01eef7a33a945
BaiCai <709931138@qq.com>
2019-04-09 15:21:06 +0800

    业务开发中

--- src/main/java/com/d2c/store/api/C_OrderController.java
+++ src/main/java/com/d2c/store/api/C_OrderController.java
@@ -382,17 +382,4 @@ public class C_OrderController extends BaseController {
         return Response.restResult(viewUrl, ResultCode.SUCCESS);
     }
 
-    @ApiOperation(value = "法大大回调接口")
-    @RequestMapping(value = "/fadada/callback", method = RequestMethod.POST)
-    public R fadadacallback(String transaction_id,String contract_id,String result_code,String result_desc,String timestamp,String msg_digest){
-        OrderQuery oq = new OrderQuery();
-        OrderDO orderDO=orderService.getOne(QueryUtil.buildWrapper(oq));
-        if(OrderDO.StatusEnum.WAIT_MEM_SIGN.name().equals(orderDO.getStatus())){
-            OrderDO order=new OrderDO();
-            order.setId(orderDO.getId());
-            order.setStatus(OrderDO.StatusEnum.WAIT_P2P_SIGN.name());
-            orderService.updateById(order);
-        }
-        return Response.restResult(null, ResultCode.SUCCESS);
-    }
 }
